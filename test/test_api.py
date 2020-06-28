cd2ab0fba1ef24e9aac6b0c26b24dea8b425978a
Julien-V <julien.vassort61@gmail.com>
2020-06-28 09:09:41 +0200

    test_api reworked

--- test/test_api.py
+++ test/test_api.py
@@ -3,8 +3,11 @@
 
 import os
 import json
+import pytest
 
+from grandpy.api.api import GetAPI
 from grandpy.api.maps_api import GMaps_API
+from grandpy.api.wiki_api import Wikipedia_API
 
 
 def j(json_file):
@@ -14,43 +17,56 @@ def j(json_file):
     return json.loads(json_file)
 
 
-class MockResponse:
-    @staticmethod
-    def get_valid():
-        return j("valid_g_maps.json")
+@pytest.fixture
+def patch_get_and_load(monkeypatch):
+    def mock_get_and_load(*args):
+        return json_file.values
+    monkeypatch.setattr(GetAPI, "get_and_load", mock_get_and_load)
 
-    @staticmethod
-    def get_invalid():
-        return j("invalid_g_maps.json")
+    class FakeJSON_file:
+        pass
 
-    @staticmethod
-    def get_no_candidates():
-        return j("no_candidates_g_maps.json")
+    json_file = FakeJSON_file()
+    json_file.values = None
+    return json_file
 
 
 class TestGMaps_API:
     g_maps = GMaps_API("http://maps.etc", {'param': "test"}, "query")
 
-    def test_run_valid(self, monkeypatch):
-        def mock_get_and_load(*args):
-            return MockResponse.get_valid()
-
-        monkeypatch.setattr(GMaps_API, "get_and_load", mock_get_and_load)
+    def test_run_valid(self, patch_get_and_load):
+        patch_get_and_load.values = j("valid_g_maps.json")
         result = self.g_maps.run()
         assert (isinstance(result['formatted_address'], str))
 
-    def test_run_invalid(self, monkeypatch):
-        def mock_get_and_load(*args):
-            return MockResponse.get_invalid()
-
-        monkeypatch.setattr(GMaps_API, "get_and_load", mock_get_and_load)
+    def test_run_invalid(self, patch_get_and_load):
+        patch_get_and_load.values = j("invalid_g_maps.json")
         result = self.g_maps.run()
         assert (result is None)
 
-    def test_run_no_candidates(self, monkeypatch):
-        def mock_get_and_load(*args):
-            return MockResponse.get_no_candidates()
-
-        monkeypatch.setattr(GMaps_API, "get_and_load", mock_get_and_load)
+    def test_run_no_candidates(self, patch_get_and_load):
+        patch_get_and_load.values = j("unknown_g_maps.json")
         result = self.g_maps.run()
+        assert (result is None)
+
+
+class TestWikipedia_API:
+    wiki = Wikipedia_API("http://wikipedia.etc", {'param': "test"}, "query")
+
+    def test_run_valid(self, patch_get_and_load):
+        patch_get_and_load.values = j("valid_wiki.json")
+        result = self.wiki.run()
+        assert (isinstance(result, dict))
+
+    def test_run_invalid(self, patch_get_and_load):
+        # a test with a different json file
+        # like valid_g_maps.json
+        # result should be None
+        patch_get_and_load.values = j("invalid_wiki.json")
+        result = self.wiki.run()
+        assert (result is None)
+
+    def test_run_unknown(self, patch_get_and_load):
+        patch_get_and_load.values = j("unknown_wiki.json")
+        result = self.wiki.run()
         assert (result is None)
